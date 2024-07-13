d8e05b3a39f61e76286c66a12baa83503f60cdc6
SultanTipa1 <josephngumbaugush@gmail.com>
2024-07-13 23:41:59 +0300

    Adds all files

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Write your code in this file!
+const currentUser = 'Joseph';
+
+const welcomeMessage = 'welcome to Flatbook, ';
+
+const welcomeMessage = currentUser;
+
+
+const welcomeMessage = 'welcome to Flatbook, currentUser';
+
+
+const welcomeMessage = 'welcome to Flatbook,' + currentUser;
+
+
+const myString = 'template literal';
+
+const myNumber = 10;
+
+const myBoolean = true;
+
+
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}';
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const currentUser = 'Joseph';
+
+const welcomeMessage = `Welcome to Flatook, ${currentUser}!`;
+
+const exitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = '';
+
+const shortGreeting = 'Welcome, ';
+
+const shortGreeting = `Welcome, ${currentUser}`;
+
+'Edsger Dijkstra'[0];
+//=> "E"
+
+'Edsger Dijkstra'.charAt(0);
+//=> "E"
+
+<!-- .slice() --!>
+
+
+'Edsger Dijkstra'.length;
+//=> 15
+
+'Edsger Dijkstra'[15];
+//=> undefined
+
+'Edsger Dijkstra'[14];
+//=> "a"
+
+'Edsger Dijkstra'.slice(0, 3);
+//=> "Eds"
+
+currentUser.slice(0, 1);
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
